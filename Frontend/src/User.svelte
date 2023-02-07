96c56bee361c51cb05a48399f3ab46f5961da32b
Ankitsingh Bisht <ankitsingh.cs18018@mmcc.edu.in>
2023-02-07 14:39:21 +0530

    backup data json added

---
+++ Frontend/src/User.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { users } from "./data-users";
+  export let user = {};
+  const deleteUser = async () => {
+    const deleteURL = `http://localhost:4000/user/` + user.id;
+    let res = await fetch(deleteURL, {
+      method: "DELETE",
+    });
+    res = await res.json();
+    console.log(res);
+    $users = $users.filter((item) => users.id != item.id);
+  };
+</script>
+
+<div class="users">
+  {user.fName}
+  {user.lName}
+  <button on:click={deleteUser}>Delete</button>
+</div>
---
+++ Frontend/src/Users.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { onMount } from "svelte";
+  import { users } from "./data-users";
+  import User from "./User.svelte";
+
+  onMount(async () => {
+    const url = "http://localhost:4000/user/";
+    let res = await fetch(url);
+    let data = await res.json();
+    $users = data;
+  });
+</script>
+
+<div id="users">
+  {#each $users as user}
+    <User {user} />
+  {/each}
+</div>
